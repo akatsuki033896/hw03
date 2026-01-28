f13c699ab79e65ff1073081cfca145b364f74945
akatsuki <akatsuki@akatsukideMacBook-Air.local>
2026-01-28 18:33:02 +0800

    Finish all request in homework

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <iostream>
+#include <type_traits>
 #include <vector>
 #include <variant>
 
 // 请修复这个函数的定义：10 分
+template <class T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
     os << "{";
     for (size_t i = 0; i < a.size(); i++) {
@@ -16,19 +19,33 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &a) {
 
 // 请修复这个函数的定义：10 分
 template <class T1, class T2>
-std::vector<T0> operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
+auto operator+(std::vector<T1> const &a, std::vector<T2> const &b) {
     // 请实现列表的逐元素加法！10 分
     // 例如 {1, 2} + {3, 4} = {4, 6}
+    // std::vector<decltype(T1() + T2())> v; // 不适用variant
+    using T0 = decltype(T1() + T2());
+    std::vector<T0> v;
+    for (size_t i = 0; i < std::min(a.size(), b.size()); i++) {
+        v.push_back(a[i] + b[i]);
+    }
+    return v;
 }
 
 template <class T1, class T2>
 std::variant<T1, T2> operator+(std::variant<T1, T2> const &a, std::variant<T1, T2> const &b) {
     // 请实现自动匹配容器中具体类型的加法！10 分
+    return std::visit([] (auto const &a, auto const &b) -> std::variant<T1, T2> {
+        return a + b;
+    }, a, b);
 }
 
 template <class T1, class T2>
 std::ostream &operator<<(std::ostream &os, std::variant<T1, T2> const &a) {
     // 请实现自动匹配容器中具体类型的打印！10 分
+    std::visit([&] (auto const &x) {
+        std::cout << x << '\n';
+    }, a);
+    return os;
 }
 
 int main() {
@@ -43,10 +60,11 @@ int main() {
 
     // 应该输出 {4.14, 6.718, 2.618}
     std::cout << c << std::endl;
+    // std::cout << typeid(c).name() << std::endl;
 
     std::variant<std::vector<int>, std::vector<double>> d = c;
     std::variant<std::vector<int>, std::vector<double>> e = a;
-    d = d + c + e;
+    d = d + decltype(d)(c) + e;
 
     // 应该输出 {9.28, 17.436, 7.236}
     std::cout << d << std::endl;
